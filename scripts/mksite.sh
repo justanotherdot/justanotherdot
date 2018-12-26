255fcca98113133e17710e0e72793856e94828e2
Ryan James Spencer <spencer.ryanjames@gmail.com>
2018-12-26 19:00:52 +1100

    Include deployment submodule

--- scripts/mksite.sh
+++ scripts/mksite.sh
@@ -1,4 +1,6 @@
-bulma_version="0.5.2"
 stack exec build-site
+#bulma_version="0.5.2"
+#curl -o dist/assets/bulma.min.css https://cdnjs.cloudflare.com/ajax/libs/bulma/"$bulma_version"/css/bulma.min.css
 mkdir dist/assets
-curl -o dist/assets/bulma.min.css https://cdnjs.cloudflare.com/ajax/libs/bulma/"$bulma_version"/css/bulma.min.css
+cp assets/bulma.min.css dist/assets/.
+cp -r dist/* deploy/.
